6def381628a8ca7eeae1a31fb6550366229dac89
ddamar798 <damarbudiprasetyo714@gmail.com>
2024-12-26 10:47:46 +0700

    feat: add feature in game

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 welcome_message = "LEARN PYTHON"
 tupai_position = random.randint(1,4)
 
+
+
 print("||============||")
 print(f"||{welcome_message}||")
 print("||============||")
@@ -12,12 +14,17 @@ print("||============||")
 #     print("Yaa nomor saya emang 3")
 # else: print("Salah nomermu bukan 3")
 
+bentuk_goa = "|_|"
+goa_kosong = [bentuk_goa] * 4 #GOA INI HARUS TETAP KOSONG.
+goa = goa_kosong.copy() #DISINI GOA TEMPAT TUPAI MUNCUL.
+
+goa[tupai_position - 1] = "|0.0|"
+
 namaUser = input("masukan namamu :")
 print(f'''
 Haii {namaUser}!
 Coba perhatikan Goa dibawah ini :
- 1    2    3    4
-|_|  |_|  |_|  |_|
+{goa_kosong}
 ''')
 
 pilihanUser = int (input("Menurutmu dimana tupai berada? [1 / 2 / 3 / 4 ]: "))
@@ -27,9 +34,9 @@ validasi_pilihan = input (f"apakah kamu yakin jawabanya adalah {pilihanUser}? [y
 
 if validasi_pilihan == "y":
     if pilihanUser == tupai_position:
-         print("Selamat anda benar!")
+         print(f"{goa} \n Selamat anda benar! Tupai berada di goa nomor {pilihanUser}")
     else: 
-         print(f"Pilihan anda salah! tupai berada di goa {tupai_position}.")
+         print(f"Pilihan anda salah!{goa} \n Tupai berada di Goa {tupai_position}")
 elif validasi_pilihan == "n":
     print("oke, program ditutup!")
     exit()
