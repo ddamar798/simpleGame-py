30d1518421a02e9e47114ed817a1a0bb4d2d1dbc
ddamar798 <damarbudiprasetyo714@gmail.com>
2024-12-27 11:08:24 +0700

    feat: add file learn while/looping

---
+++ while.py
@@ -0,0 +1,9 @@
+start = 0
+object = "Agus"
+
+while start < 5:
+    print(object)
+    start +=1
+    
+    # MELAKUKAN PENGULANGAN, JIKA START < DARIPADA 5 MAKA PRINT OBJECT,
+    # SETELAH ITU START +=1 AGAR TIDAK TERJADI INFINITY LOOP.
